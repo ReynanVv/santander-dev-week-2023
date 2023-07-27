5c645d8c24bf6ffe7f18ad5106c5d8b54819f08c
Reynan Vieira <reynanvt@gmail.com>
2023-07-27 11:16:21 -0300

    Initial commit

---
+++ src/test/java/br/inova/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
